b64e33a34a4edda4a277996fee0b8a598d21a172
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-07 10:52:43 +0100

    feat: add title, subtitle and apply correct classes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,8 +5,18 @@ import { LoginForm } from "../components";
 const Login = () => {
   return (
     <div className="login">
-      <h1>Login</h1>
-      <LoginForm />
+      <div className="login__hero">
+        <h1>Welcome back boss!</h1>
+      </div>
+
+      <div className="login__form">
+        <div className="login__form--title">
+          <h2>Login</h2>
+          <p>Welcome back, please login below to go to the admin panel.</p>
+        </div>
+
+        <LoginForm />
+      </div>
     </div>
   );
 };
