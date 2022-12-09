bd6591596ee7f359e9bfcfe8497b8f59564ea229
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-09 16:46:55 +0100

    feat: check if token is set in local storage

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Gallery = () => {
+  // check if user is logged in
+  const token = localStorage.getItem("token");
+  if (!token) {
+    window.location.href = "/";
+  } else {
+    console.log("logged in");
+  }
+
   return <div>Gallery</div>;
 };
 
