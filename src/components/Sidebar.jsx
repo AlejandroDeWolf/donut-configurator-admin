2dd343182db84c539145f254775eb634f3739e76
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-10 21:04:51 +0100

    feat: create sidebar with two menu items

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai'
+import { IconContext } from 'react-icons'
+
+const Sidebar = () => {
+    const [sidebar, setSidebar] = useState(false)
+
+    const showSidebar = () => setSidebar(!sidebar)
+
+    return (
+        <IconContext.Provider value={{ color: "undefined" }}>
+            <div className='sidebar'>
+                <Link to='#' className='menu-bars'>
+                    <FaIcons.FaBars onClick={showSidebar} />
+                </Link>
+            </div>
+
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                        <Link to='#' className='menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </li>
+                    <li className='nav-text'>
+                        <Link to='/dashboard'>
+                            <AiIcons.AiFillHome />
+                            <span>Dashboard</span>
+                        </Link>
+                    </li>
+                    <li className='nav-text'>
+                        <Link to='/profile'>
+                            <AiIcons.AiFillHome />
+                            <span>Profile</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </IconContext.Provider>
+    )
+}
+
+export default Sidebar
