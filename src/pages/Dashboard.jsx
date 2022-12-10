597e840eb29215b51d57fc42cf338e8555add4d0
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-10 17:15:00 +0100

    feat: add sidebar component to dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Sidebar } from "../components";
+
 const Dashboard = () => {
   // check if user is logged in
   const token = localStorage.getItem("token");
@@ -9,7 +11,17 @@ const Dashboard = () => {
     console.log("logged in");
   }
 
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <Sidebar />
+      <div className="dashboard__content">
+        <div className="dashboard__content__title">
+          <h2>Dashboard</h2>
+          <p>Welcome back, please login below to go to the admin panel.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
