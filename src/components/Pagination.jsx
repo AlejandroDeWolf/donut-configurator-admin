bc1d261d3fb37dd5c19fecfb65c0004a47dce00d
AimaneMaadal <aimanemaadal@gmail.com>
2022-12-11 16:35:06 +0100

    feat: create pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Pagination = ({ donutsPerPage, totalDonuts, paginate }) => {
+    // create pagination and highlight current page
+    const pageNumbers = []
+
+    for (let i = 1; i <= Math.ceil(totalDonuts / donutsPerPage); i++) {
+        pageNumbers.push(i)
+    }
+
+    // start with first page highlighted
+    const activePage = (e) => {
+        const paginationItems = document.querySelectorAll('.pagination__item__link')
+        paginationItems.forEach(item => {
+            item.classList.remove('active')
+        })
+        e.target.classList.add('active')
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(number => (
+                    <li key={number} className="pagination__item">
+                        <button onClick={(e) => { paginate(number); activePage(e) }} className="pagination__item__link">{number}</button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
