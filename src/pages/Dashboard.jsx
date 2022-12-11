45036df84cca4e617a24556ed18746ff6a86cc75
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-11 15:56:44 +0100

    feat: fetch all donuts and pass data as props to donuts component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Sidebar } from "../components";
+import { Donuts, Sidebar } from "../components";
 
 const Dashboard = () => {
   // check if user is logged in
@@ -11,14 +11,31 @@ const Dashboard = () => {
     console.log("logged in");
   }
 
+  // fetch donut data from api
+  const [donuts, setDonuts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDonuts = async () => {
+      const response = await fetch(
+        "https://adorable-red-sundress.cyclic.app/donuts"
+      );
+      const data = await response.json();
+      setDonuts(data);
+      setLoading(false);
+    };
+    fetchDonuts();
+  }, []);
+
   return (
     <div className="container__dashboard">
       <Sidebar />
-      <div className="dashboard">
+      <div className="dashboard section__padding">
         <div className="dashboard__content">
           <div className="dashboard__content__title">
             <h2>Dashboard</h2>
           </div>
+          <Donuts donuts={donuts} loading={loading} />
         </div>
       </div>
     </div>
