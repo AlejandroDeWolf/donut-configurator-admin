7caa4abc42475f9db268a402d4d25d35ac00d0d9
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-12 15:26:59 +0100

    feat: add link to 3D donut

--- src/components/UI/DonutModal.jsx
+++ src/components/UI/DonutModal.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 import Modal from "@mui/material/Modal";
 
-// modal component
 const DonutModal = ({ modalData, open, handleClose }) => {
+  const donutUrl = `https://donutello.netlify.app/donut.html?id=${modalData._id}`;
+
   return (
     <Modal open={open} onClose={handleClose}>
       <div className="modal">
@@ -12,6 +13,10 @@ const DonutModal = ({ modalData, open, handleClose }) => {
             <h2>Bedrijf: {modalData.company}</h2>
             <p>Datum: {modalData.date}</p>
             <p>Extra: {modalData.remarks}</p>
+
+            <a href={donutUrl} target="_blank" rel="noreferrer" className="btn">
+              Bekijk donut
+            </a>
 
             <button onClick={handleClose} className="btn btn__form">
               Close
