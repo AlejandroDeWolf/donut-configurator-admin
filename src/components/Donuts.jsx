5291a8b14cb1f678a61a6f9a1ac2b9a84a724481
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-12 13:25:20 +0100

    feat: add modal component and pass state as props

--- src/components/Donuts.jsx
+++ src/components/Donuts.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import DonutModal from "./UI/DonutModal";
 
 const Donuts = ({ donuts, loading }) => {
-    if (loading) {
-        return <h1>Loading...</h1>
-    } else {
-        console.log(donuts)
-    }
+  if (loading) {
+    return <h1>Loading...</h1>;
+  } else {
+    console.log(donuts);
+  }
 
-    return (
-        <div className="dashboard__content__donuts">
-            {donuts.map((donut) => (
-                <div className="dashboard__content__donuts__donut" key={donut._id}>
-                    <div className="dashboard__content__donuts__donut__image">
-                        <img src={donut.snapshot} alt="donut" />
-                    </div>
-                    <div className="dashboard__content__donuts__donut__info">
-                        <h3>{donut.company}</h3>
-                        <p>{donut.date}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    )
-}
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [open, setOpen] = useState(false);
+  const [modalData, setModalData] = useState({});
 
-export default Donuts
+  return (
+    <>
+      <DonutModal
+        open={open}
+        handleClose={handleClose}
+        modalData={modalData}
+        className="modal__container"
+      />
+      <div className="dashboard__content__donuts">
+        {donuts.map((donut) => (
+          <div
+            className="dashboard__content__donuts__donut"
+            key={donut._id}
+            onClick={() => {
+              handleOpen();
+              setModalData(donut);
+            }}
+          >
+            <div className="dashboard__content__donuts__donut__image">
+              <img src={donut.snapshot} alt="donut" />
+            </div>
+            <div className="dashboard__content__donuts__donut__info">
+              <h3>{donut.company}</h3>
+              <p>{donut.date}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Donuts;
