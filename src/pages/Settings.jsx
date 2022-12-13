be3c65bdef7f114ecdbef7224b8361d03c58d802
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-13 15:36:42 +0100

    feat: add loginform component to settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Sidebar } from "../components";
+import { SettingsForm } from "../components";
 
 const Settings = () => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    window.location.href = "/";
+  } else {
+    console.log("logged in");
+  }
+
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { oldPassword, newPassword } = this.state;
+
+    fetch("http://localhost:5000/api/users/update-password", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ oldPassword, newPassword }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
-      <Sidebar />
-      <div className="settings">
-        <div className="settings__content">
-          <div className="settings__content__title">
-            <h2>Settings</h2>
+      <div className="wrapper">
+        <Sidebar />
+        <div className="wrapper__content section__padding">
+          <div className="settings">
+            <div className="settings__title">
+              <h1>Settings</h1>
+            </div>
+            <div className="settings__form">
+              <SettingsForm />
+            </div>
           </div>
         </div>
       </div>
