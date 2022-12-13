3388d40f9e08f23dda1de94f7a997b30867cbf53
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-13 11:53:16 +0100

    feat: expand donut modal

--- src/components/UI/DonutModal.jsx
+++ src/components/UI/DonutModal.jsx
@@ -10,18 +10,38 @@ const DonutModal = ({ modalData, open, handleClose }) => {
       <div className="modal">
         <div className="modal__content">
           <div className="modal__content__header">
-            <h2>Bedrijf: {modalData.company}</h2>
-            <p>Datum: {modalData.date}</p>
-            <p>Extra: {modalData.remarks}</p>
-
-            <a href={donutUrl} target="_blank" rel="noreferrer" className="btn">
-              Bekijk donut
-            </a>
-
-            <button onClick={handleClose} className="btn btn__form">
-              Close
-            </button>
+            <h2>{modalData.company}</h2>
+            <div className="modal__content__image">
+              <img src={modalData.snapshot} alt="" />
+            </div>
           </div>
+          <div className="modal__content__data">
+            <div className="modal__content__data__date modal__content__data--flex">
+              <span>Datum:</span>
+              <p>{modalData.date}</p>
+            </div>
+            <div className="modal__content__data__quantity modal__content__data--flex">
+              <span>Aantal:</span>
+              <p>100</p>
+            </div>
+            <div className="modal__content__data__quantity modal__content__data--flex--long">
+              <span>E-mail:</span>
+              <p>dewolf.alejandro@gmail.com</p>
+            </div>
+            <div className="modal__content__data__remarks modal__content__data--flex--long">
+              <span>Extra:</span>
+              <p>{modalData.remarks}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="modal__buttons">
+          <button onClick={handleClose} className="btn btn--secondary">
+            Close
+          </button>
+          <a href={donutUrl} target="_blank" rel="noreferrer" className="btn">
+            Bekijk donut
+          </a>
         </div>
       </div>
     </Modal>
