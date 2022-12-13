f175659da15ddab42a3d2b149b47d5ac89859ba7
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-13 19:11:15 +0100

    feat: temporarly disable login form component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,66 +1,9 @@
 import React from "react";
-import { useRef } from "react";
 
 const Form = () => {
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-
-  const fetchLogin = async (e) => {
-    e.preventDefault();
-
-    // get username and password from form
-    const username = usernameRef.current.value;
-    const password = passwordRef.current.value;
-
-    const response = await fetch(
-      "https://adorable-red-sundress.cyclic.app/users/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      }
-    );
-
-    // error handling
-    if (!response.ok) {
-      const error = await response.json();
-      console.log(error);
-      console.log(username);
-      return;
-    } else {
-      const data = await response.json();
-      console.log(data);
-      // store token in local storage
-      localStorage.setItem("token", data.token);
-      // redirect to admin page
-      window.location.href = "/dashboard";
-    }
-  };
-
   return (
-    <form>
-      <div className="form__group">
-        <label htmlFor="username">Gebruikersnaam</label>
-        <input type="text" name="username" id="username" ref={usernameRef} />
-      </div>
-      <div className="form__group">
-        <label htmlFor="password">Wachtwoord</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          ref={passwordRef}
-        />
-      </div>
-      <button type="submit" className="btn form__btn" onClick={fetchLogin}>
-        Login
-      </button>
-    </form>
+    <>
+    </>
   );
 };
 
