482aee283f240c0bbb2937d89a3be95e761eb3d5
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-12-13 18:13:58 +0100

    refactor: translate content to dutch

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,13 +6,14 @@ const Login = () => {
   return (
     <div className="login">
       <div className="login__hero">
-        <h1>Welcome back boss!</h1>
+        <h1>Welcome back <span className="login__hero__highlight">boss</span>!</h1>
       </div>
 
       <div className="login__form">
         <div className="login__form__title">
           <h2>Login</h2>
-          <p>Welcome back, please login below to go to the admin panel.</p>
+          <p>Welkom terug! Gelieve even in te loggen met
+            jouw gegevens om verder te gaan naar het dashboard.</p>
         </div>
 
         <LoginForm />
