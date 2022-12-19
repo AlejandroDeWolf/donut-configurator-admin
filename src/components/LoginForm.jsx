30e351f571d9312727ed529bb8a9cd4586343f26
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-19 16:02:59 +0100

    refactor: show error message above form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { Oval } from 'react-loader-spinner'
+import { Oval } from "react-loader-spinner";
 
 const LoginForm = () => {
   const usernameRef = useRef();
@@ -38,34 +38,48 @@ const LoginForm = () => {
 
   return (
     <div className="login__form">
-      {loading &&
+      {loading && (
         <div className="login__form__loader">
           <Oval
             height={80}
             width={80}
             color="#e72c70"
             visible={true}
-            ariaLabel='oval-loading'
+            ariaLabel="oval-loading"
             secondaryColor="#f5659a"
             strokeWidth={18}
             strokeWidthSecondary={18}
           />
         </div>
-      }
+      )}
 
       {/* if not loading show form with error handling */}
 
-      {!loading &&
+      {!loading && (
         <div>
           <div className="login__form__title">
             <h2>Login</h2>
-            <p>Welkom terug! Gelieve even in te loggen met
-              jouw gegevens om verder te gaan naar het dashboard.</p>
+            <p>
+              Welkom terug! Gelieve even in te loggen met jouw gegevens om
+              verder te gaan naar het dashboard.
+            </p>
           </div>
+
+          {error && (
+            <div className="form__error">
+              <p>Gebruikersnaam of wachtwoord is niet correct.</p>
+            </div>
+          )}
+
           <form>
             <div className="form__group">
               <label htmlFor="username">Gebruikersnaam</label>
-              <input type="text" name="username" id="username" ref={usernameRef} />
+              <input
+                type="text"
+                name="username"
+                id="username"
+                ref={usernameRef}
+              />
             </div>
             <div className="form__group">
               <label htmlFor="password">Wachtwoord</label>
@@ -77,16 +91,16 @@ const LoginForm = () => {
               />
             </div>
 
-            {error && <div className="form__error">
-              <p>Gebruikersnaam of wachtwoord is niet correct.</p>
-            </div>}
-
-            <button type="submit" className="btn form__btn" onClick={fetchLogin}>
+            <button
+              type="submit"
+              className="btn form__btn"
+              onClick={fetchLogin}
+            >
               Login
             </button>
           </form>
         </div>
-      }
+      )}
     </div>
   );
 };
