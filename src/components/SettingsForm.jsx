7cc09169b51d8aaafa2dba7abfc78c7695bacb3f
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-12-19 19:03:56 +0100

    feat: password update and error message

--- src/components/SettingsForm.jsx
+++ src/components/SettingsForm.jsx
@@ -2,60 +2,86 @@ import React from "react";
 import { useRef, useState } from "react";
 
 const SettingsForm = () => {
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
+  const [error, setError] = useState(false);
 
-  const handleSubmit = (e) => {
+  const password = useRef();
+  const newPassword = useRef();
+
+  const token = localStorage.getItem("token");
+
+  const updatePassword = async (e) => {
     e.preventDefault();
-    const { oldPassword, newPassword } = this.state;
 
-    fetch("http://localhost:5000/api/users/update-password", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ oldPassword, newPassword }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => console.log(err));
+    const passwordValue = password.current.value;
+    const newPasswordValue = newPassword.current.value;
+
+    const response = await fetch(
+      "https://adorable-red-sundress.cyclic.app/users/update",
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          oldPassword: passwordValue,
+          newPassword: newPasswordValue,
+          token: token,
+        }),
+      }
+    );
+    const data = await response.json();
+    if (data.status === 200) {
+      console.log("password updated");
+    } else {
+      setError(true);
+    }
   };
 
-  //   logout by removing token from local storage
   const logoutHandler = () => {
     localStorage.removeItem("token");
     window.location.href = "/";
   };
 
   return (
-    <form>
-      {/* update password */}
-      <div className="form__group form__group--small">
-        <label htmlFor="password">Wachtwoord</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      {/* update password confirmation */}
-      <div className="form__group form__group--small">
-        <label htmlFor="password">Wachtwoord bevestigen</label>
-        <input type="password" name="password" id="passwordUpdate" />
-      </div>
+    <>
+      {error && (
+        <div className="form__error form__error--settings">
+          <p>Er is iets misgegaan, probeer het opnieuw.</p>
+        </div>
+      )}
+      <form>
+        <div className="form__group form__group--small">
+          <label htmlFor="password">Huidig wachtwoord</label>
+          <input type="password" name="password" id="password" ref={password} />
+        </div>
+        <div className="form__group form__group--small">
+          <label htmlFor="password">Nieuw wachtwoord</label>
+          <input
+            type="password"
+            name="password"
+            id="passwordUpdate"
+            ref={newPassword}
+          />
+        </div>
 
-      {/* logout en update wachtwoord button */}
-      <div className="form__group__buttons">
-        <button
-          type="submit"
-          className="btn btn--secondary"
-          onClick={logoutHandler}
-        >
-          Uitloggen
-        </button>
-        <button type="submit" className="btn btn__logout">
-          Wijzig wachtwoord
-        </button>
-      </div>
-    </form>
+        <div className="form__group__buttons">
+          <button
+            type="submit"
+            className="btn btn--secondary"
+            onClick={logoutHandler}
+          >
+            Uitloggen
+          </button>
+          <button
+            type="submit"
+            className="btn btn__logout"
+            onClick={updatePassword}
+          >
+            Wijzig wachtwoord
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
